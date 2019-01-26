c65b1c1aac5f886803a1f433cf1cc8c83ab8d2f8
Mert Kaya <30465847+sertcast@users.noreply.github.com>
2019-01-25 23:47:37 -0500

    added the basic structure and a pdb file

---
+++ pdb_parse/pdb_parse.c
@@ -0,0 +1,9 @@
+//
+//  pdb_parse.c
+//  pdb_parse
+//
+//  Created by Mert Kaya on 2019-01-25.
+//  Copyright © 2019 Mert Kaya. All rights reserved.
+//
+
+#include "pdb_parse.h"
---
+++ pdb_parse/pdb_parse.h
@@ -0,0 +1,46 @@
+//
+//  pdb_parse.h
+//  pdb_parse
+//
+//  Created by Mert Kaya on 2019-01-25.
+//  Copyright © 2019 Mert Kaya. All rights reserved.
+//
+
+#ifndef pdb_parse_h
+#define pdb_parse_h
+
+#include <stdio.h>
+
+typedef struct Atom{
+    int ID;
+    char name[5];
+    double coor[3];
+    struct Residue* res;
+} Atom;
+
+typedef struct Residue{
+    int ID;
+    char name[4];
+    Atom* atoms;
+    int num_atoms;
+    struct Chain* chains;
+} Residue;
+
+typedef struct Chain{
+    char ID;
+    Residue* residues;
+    int num_residues;
+    struct Protein* protein;
+} Chain;
+
+typedef struct Protein{
+    char* name;
+    Atom* atoms;
+    Residue* residues;
+    Chain* chains;
+    int num_atoms;
+    int num_residues;
+    int num_chains;
+} Protein;
+
+#endif /* pdb_parse_h */
